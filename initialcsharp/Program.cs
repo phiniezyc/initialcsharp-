422cf9e7308f9b9ec5dd10ac816c7dced6d298ad
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-03 21:30:09 -0500

    update w/ debug file

--- initialcsharp/Program.cs
+++ initialcsharp/Program.cs
@@ -6,7 +6,7 @@ namespace initialcsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!\nAloha");
         }
     }
 }
