c83df33a2ef9a172e31dfb774001f2c0b07ab3e5
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-05 23:46:09 -0500

    Update writeline

--- initialcsharp/Program.cs
+++ initialcsharp/Program.cs
@@ -2,14 +2,17 @@
 
 namespace initialcsharp
 {
-    using System.IO;
-    class Program
+  using System.IO;
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string [] lines = { "This is a test", "It work!", "Yep!" };
-            File.WriteAllLines("csharptest.txt", lines);
-            Console.WriteLine("Hello World!!!\nAloha NCR");
-        }
+      string[] lines = { "This is a test", "It work!", "Yep!" };
+      File.WriteAllLines("csharptest.txt", lines);
+      Console.WriteLine("Hello World!!!\nAloha Engineering");
+
+      string[] fileContents = File.ReadAllLines("csharptest.txt");
+      Console.WriteLine(fileContents);
     }
+  }
 }
