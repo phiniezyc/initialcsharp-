f218bb8667376ccfa9ed844dc9be4232706e52b0
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-07 21:05:35 -0500

    Change writeline

--- initialcsharp/Program.cs
+++ initialcsharp/Program.cs
@@ -9,7 +9,7 @@ namespace initialcsharp
     {
       string[] lines = { "This is a test", "It work!", "Yep!" };
       File.WriteAllLines("csharptest.txt", lines);
-      Console.WriteLine("Hello World!!!\nAloha Engineering");
+      Console.WriteLine("Hello World!\nAloha Engineering");
 
       string[] fileContents = File.ReadAllLines("csharptest.txt");
       Console.WriteLine(fileContents);
