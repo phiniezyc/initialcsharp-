9b6ac091f356f5fc7d120db17b977a0f5bdc5073
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-19 15:39:37 -0500

    Add 3rd class boilerplate

---
+++ initialcsharp/Third_Class.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Second_Program
+{
+  class Second_Class
+  {
+    public void Second(string message)
+    {
+      Console.WriteLine("2nd class is connected and is this is the message: {0}", message);
+    }
+  }
+}
