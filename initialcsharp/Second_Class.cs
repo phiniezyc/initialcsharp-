5bc0164ac69b6ccaf9c6bc3177430f8f3738a0d2
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-21 22:08:45 -0500

    Add 3rd class log

--- initialcsharp/Second_Class.cs
+++ initialcsharp/Second_Class.cs
@@ -8,6 +8,8 @@ namespace Second_Program
     public void Second(string message)
     {
       Console.WriteLine("2nd class is connected and is this is the message: {0}", message);
+      Second_Program.Second_Class test_import = new Second_Class();
+      test_import.Second("yes!");
     }
   }
 }
