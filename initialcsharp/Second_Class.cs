e2807c3ffbc78ee8bed89266d07dfbecf0cf769a
Chance Phiniezy <phiniezyc@gmail.com>
2020-01-22 22:04:30 -0500

    Change class to add message

--- initialcsharp/Second_Class.cs
+++ initialcsharp/Second_Class.cs
@@ -9,7 +9,7 @@ namespace Second_Program
     {
       Console.WriteLine("2nd class is connected and is this is the message: {0}", message);
       Third_Program.Third_Class test_import = new Third_Class();
-      test_import.Third("This is the 3rd class");
+      test_import.Third("This is the 3rd class. You are connected!");
     }
   }
 }
